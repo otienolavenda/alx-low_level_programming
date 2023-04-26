3ad9be2d3abc1dd52d394af8b3adf34a612a70fb
lavenda ottieno <otienolavenda@gmai.com>
2023-04-26 12:01:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase followed by newline
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+		_putchar(c++);
+	_putchar('\n');
+}
