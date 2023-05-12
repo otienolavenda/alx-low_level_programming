28e9119738cbf058cd6b81e223f07e1185e5370d
lavenda ottieno <otienolavenda@gmai.com>
2023-05-12 05:45:33 -0700

    101-wildcmp.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_h
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -7,3 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2)
+
+#endif	
