e20144723d073f1572320877ba6913e557187948
lavenda ottieno <otienolavenda@gmai.com>
2023-05-16 06:04:29 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenate `s1' and `s2' and return pointer to new string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int len1, len2, size, i, j;
+	char *nstr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+
+	size = len1 + len2;
+	nstr = malloc((sizeof(char) * size) + 1);
+	/*check if malloc was successful */
+	if (nstr == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < len1)
+	{
+		nstr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i <= size)
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (nstr);
+}
