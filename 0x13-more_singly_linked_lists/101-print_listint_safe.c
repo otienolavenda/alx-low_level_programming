0d90f20f4c859bc5b26dbdfe325afcd50e78a1f0
lavenda ottieno <otienolavenda@gmai.com>
2023-06-07 01:39:58 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,42 +1,69 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a list
- * @head: pointer
- * Return: the number of nodes
+ * free_listp - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *first, *last;
-	size_t count = 0;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
 
-	first = head;
-	last = head;
-
-	if (!head)
-		exit(98);
-
-	while (first && last && last->next)
+	hptr = NULL;
+	while (head != NULL)
 	{
-		first = first->next;
-		last = last->next->next;
-		count++;
+		new = malloc(sizeof(listp_t));
 
-		if (first == last)
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			printf("[%p] %d\n", (void *)first, first->n);
-			first = first->next;
-			while (first != last)
+			add = add->next;
+			if (head == add->p)
 			{
-				printf("[%p] %d\n", (void *)first, first->n);
-				first = first->next;
-				count++;
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
 			}
-			printf("[%p] %d\n", (void *)first, first->n);
-			printf("-> [%p] %d\n", (void *)last, last->n);
-			return (count);
 		}
-		printf("[%p] %d\n", (void *)first, first->n);
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
 	}
-	return (count);
+
+	free_listp(&hptr);
+	return (nnodes);
 }
