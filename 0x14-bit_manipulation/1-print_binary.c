9f08a75aa88980633d7d4c5d5ae77313653a76a5
lavendaotieno <otienolavenda98@gmail.com>
2023-07-07 07:26:08 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,25 @@
 #include "main.h"
 /**
- * print_binary - prints binary equivalent of a number
- * @n: the number
- * Return: void
+ * _print_binary - recursively print rightmost bit of non-zero number
+ * in its binary representation.
+ * @n: number to print
  */
 void _print_binary(unsigned long int n)
 {
-	if (n > 1)
-	{
-		print_binary(n >> 1);
-	}
-	_putchar((n & 1) + '0');
+        if (n == 0)
+                return;
+        _print_binary(n >> 1);
+        _putchar((n & 1) + '0');
+}
+
+/**
+ * print_binary - print the binary representation of a number `n'
+ * @n: number to represent
+ */
+void print_binary(unsigned long int n)
+{
+        if (n == 0)
+                _putchar('0');
+        else
+		_print_binary(n);
 }
