6f6619c4296189ad7a840bd162c78d11015c7567
lavendaotieno <otienolavenda98@gmail.com>
2023-07-10 12:39:10 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,111 +1,55 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _errors - print error
- * @error: exit value
- * @s: filename
- * @fd: file descriptor
- * Return: 0
+ * main - program to copy
+ * @ac: argument count
+ * @av: array of arguments
+ * Return: a value
  */
-int _errors(int error, char *s, int fd)
+int main(int ac, char **av)
 {
-	switch (error)
+	int fdFrum, fdToo, wrote, readed;
+	char buff[1024];
+
+	if (ac != 3)
 	{
-		case 97:
-			fprintf(stderr, "Usage: cp file_from file_to\n");
-			exit(error);
-		case 98:
-			fprintf(stderr, "Error: Can't read from file %s\n", s);
-			exit(error);
-		case 99:
-			fprintf(stderr, "Error: Can't write to %s\n", s);
-			exit(error);
-		case 100:
-			fprintf(stderr, "Error: Can't close fd %d\n", fd);
-			exit(error);
-		default:
-			return (0);
-	}
-}
-
-/**
- * buffering - allocate space
- * @file: file
- * Return: buffer
- */
-char *buffering(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-	if (!buffer)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		exit(99);
-	}
-	return (buffer);
-}
-
-/**
- * close_file - close
- * @fc: file
- * Return: int
- */
-void close_file(int fc)
-{
-	int c;
-
-	c = close(fc);
-	if (c == -1)
-	{
-		fprintf(stderr, "Error: Can't close fd %d\n", fc);
-		exit(100);
-	}
-}
-
-/**
- * main - copy
- * @argc: num of args
- * @argv: vector
- * Return: int
- */
-int main(int argc, char *argv[])
-{
-	int file_from, file_to, reed, rite;
-	char *buffer;
-	mode_t mode = umask(0);
-
-	mode = 0664;
-
-	if (argc != 3)
-	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = buffering(argv[2]);
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
-	reed = read(file_from, buffer, 1024);
-	while (rite > 0)
+	fdFrum = open(av[1], O_RDONLY);
+	if (fdFrum == -1)
 	{
-		if (file_from == -1 || reed == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fdToo == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while ((readed = read(fdFrum, buff, 1024)) > 0)
+	{
+		wrote = write(fdToo, buff, readed);
+		if (wrote == -1)
 		{
-			fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-		rite = write(file_to, buffer, reed);
-		if (file_to == -1 || rite == -1)
-		{
-			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-			free(buffer);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-		reed = read(file_from, buffer, 1024);
-		file_to = open(argv[2], O_WRONLY | O_APPEND);
 	}
-	free(buffer);
-	close_file(file_from);
-	close_file(file_to);
+	if (readed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (close(fdFrum) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrum);
+		exit(100);
+	}
+	if (close(fdToo) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdToo);
+		exit(100);
+	}
 	return (0);
 }
