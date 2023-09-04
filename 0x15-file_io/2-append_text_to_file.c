f06ffeec1392d916ee3bf1e2a37ff58eee120622
otienolavenda <otienolavenda98@gmail.com>
2023-09-04 12:37:53 -0700

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,36 +1,32 @@
 #include "main.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
- * append_text_to_file - that appends text at the end of a file
- * @filename: variable pointer
- * @text_content: content file
- * Description: function that appends text at the end of a file
- * Return: 1 on success, -1 on failure*
+ * append_text_to_file - append text `text_content' to the end of
+ * file `filename'.
+ * @filename: name of the file to append to
+ * @text_content: content to append to `filename'
  *
- *
+ * Return: 1 on success, -1 if `write' or `open' fails
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, a;
+	int fd, size;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-
-	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1)
-	{
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
 		return (-1);
-	}
-	if (text_content == NULL)
-	{
-		return (1);
-	}
-	for (a = 0; text_content[a]; a++)
+	for (size = 0; text_content && text_content[size]; size++)
 		;
-	write(file, text_content, a);
-	close(file);
+	if (write(fd, text_content, size) != size)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
 	return (1);
 }
